43365f0ee083599fd4fc8c6b624a8de700a190dc
Keisuke Nakama <keisuke.nakama@eys-style.com>
2018-06-08 10:19:24 +0900

    delete byebug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,5 @@ class HomeController < ApplicationController
   def index
     @queries = Query.includes(:category).limit(10).order(:updated_at).reverse_order
     @categories = Category.all.order(:id)
-    byebug
   end
 end
