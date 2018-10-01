77b7c5029dff9bf7bc709d2d7783849dada57003
Keisuke Nakama <keisuke.nakama@eys-style.com>
2018-10-01 12:39:04 +0900

    bug fix 'cannot get first row data'

--- app/service/output_file_service.rb
+++ app/service/output_file_service.rb
@@ -3,7 +3,6 @@ class OutputFileService
   def self.get_csv_data input_data, has_header=false
     csv_data = CSV.generate("", :headers => input_data.columns, :write_headers => has_header) do |csv|
       input_data.each_with_index do |record, index|
-        next if index == 1
         csv << record
       end
     end
@@ -12,7 +11,6 @@ class OutputFileService
   def self.get_tsv_data input_data, has_header=false
     tsv_data = CSV.generate("", :headers => input_data.columns, :write_headers => has_header, :col_sep => "\t") do |tsv|
       input_data.each_with_index do |record, index|
-        next if index == 1
         tsv << record
       end
     end
